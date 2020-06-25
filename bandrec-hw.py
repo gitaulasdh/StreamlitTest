fbc4b308c8b17f585443f49d275d67d78b5c9f17
Mortz76 <37560937+Mortz76@users.noreply.github.com>
2020-06-25 18:47:05 -0300

    Update bandrec-hw.py

--- bandrec-hw.py
+++ bandrec-hw.py
@@ -1,13 +1,5 @@
 import streamlit as st
 import pandas as pd
-from scipy.spatial.distance import cosine
-import numpy as np
-from scipy.sparse import csr_matrix
-import string
-from fuzzywuzzy import fuzz
-from sklearn.neighbors import NearestNeighbors
-import warnings
-warnings.filterwarnings("ignore")
 
 
 # Text/Title
